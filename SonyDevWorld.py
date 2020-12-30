eb3b0740d2bec58b74cec9fb93da5ddc4ca465ed
ix5 <ix5@users.noreply.github.com>
2020-12-30 16:37:28 +0100

    SonyDevWorld: Rework laststatus, timeouts, filter
    Save last 3 posted items rather than only one in order to be
    more resilient in case a post is deleted.
    
    Filter out any post that is not tagged:
    - `xperia-open-source-archives`
    - `software-binaries`
    
    Use 0.125s short timeouts and 40s every 12th post to
    Telegram API.

--- SonyDevWorld.py
+++ SonyDevWorld.py
@@ -10,12 +10,13 @@ from os import getenv as os_getenv
 DEBUG = os_getenv("DEBUG") == "True"
 OFFLINE = os_getenv("OFFLINE") == "True"
 
-max_entries = 200
-timeout = 10.0  # Could lower to 0.125 eventually
+max_entries = 50
+timeout = 0.125
+long_timeout = 40.0
 
 print('Starting SonyDevWorld Telegram Bot python script...')
 last_file = 'laststatus.txt'  # Last published update
-last_status = None
+last_status = [None]
 # Example token: 1234567890:ABCDb1o0-ASDKHKJ3oiavKJDSFKJHkjwebb
 token = os_getenv('SONYDEVWORLD_BOT_TOKEN')
 # Example channel: -1000000000000 for t.me/MyExampleChannel
@@ -32,7 +33,7 @@ url = '{}/api/post-resources/fetch-posts/?resourceIs=file-download&limit={}'\
 
 try:
     with open(last_file, 'r') as f:
-        last_status = f.readline().strip()
+        last_status = f.read().splitlines()
 except FileNotFoundError:
     print("{0} not found, starting from scratch".format(last_file))
 
@@ -47,30 +48,54 @@ else:
         print('Failed to connect! Exception: {}'.format(e))
         sys.exit(1)
     items = json.loads(r.text)
-    # Do this once
+    # Do this once for working offline:
     #with open('items.json', 'w') as f:
     #    json.dump(items, f)
+    #with open('items-pretty.json', 'w') as f:
+    #    prettified = json.dumps(items, indent=4)
+    #    f.write(prettified)
 
 x = 0
 check = True
 while check:
-    if x == len(items):
-        # Can happen if too many entries were pushed (unrealistic) or saved
-        # entry was taken down... Too bad!
+    if x == len(items) and None in last_status:
+        # Can happen if too many entries were pushed (unrealistic) or 3
+        # consecutive items at once were taken down (even more unrealistic)
+        # We can deal with this in two ways:
+        # Option A - Post all items:
         print("Saved state not found in response, posting all items")
         break
+        # Option B - Bail out
+        #print("Saved state not found in response, maybe item was taken down?")
+        #print("Exiting")
+        #sys.exit(1)
     guid = items[x]['guid']  # e.g. 'file-download-798601'
-    check = guid != last_status
+    check = guid not in last_status
     if check:
         x += 1
     else:
-        print("Found last item, continuing where we last left off: {}{}"
+        print("Found last posted item, continuing after we last left off: {}{}"
               .format(base_url, items[x]['permalink']))
+        items = items[:x]
 
+# Filter out entries that are not OSS archives or blobs,
+# and also unpublished entries which have no tags
+items = filter(lambda x: x.get("tags"), items)
+items = list(reversed(list(filter(
+    lambda x: x["tags"][0].get("slug") in [
+        "xperia-open-source-archives",
+        "software-binaries"],
+    items))))
+
+if len(items) == 0:
+    print("No new items!")
+    sys.exit(0)
+
+last_guids = []
 t = 0
-for n in reversed(range(0, x)):
+for n in range(0, len(items)):
     post = items[n]
-    tags = post['tags']
+    tags = post.get('tags')
     title = post['post_title']
     link = base_url + post['permalink']
     if len(post['vc_content']):  # Do not try to understand it
@@ -85,20 +110,13 @@ for n in reversed(range(0, x)):
         .format(description, size, link, title)
     message = message.replace('<p>', '')  # I do not remember why we need this
     message = message.replace('</p>', '')
-    # Probably because Sony added <p> tags to description and
-    # Telegram was messing up the HTML parsing (see below)
-    if not tags:
-        # Indicate that we found an unpublished entry
-        if DEBUG:
-            print('Skipping unpublished entry: {}'.format(link))
-        continue
 
-    t += 1
     # Take longer timeout before every 12th post to TG API
     # (Be even more cautious of being throttled)
+    t += 1
     if t % 12 == 0:
-        print("Cooling down a bit, sleeping for 100s")
-        sleep(100.0)
+        print("Cooling down a bit, sleeping for {}s".format(long_timeout))
+        sleep(long_timeout)
 
     print("Posting item '{}' to TG".format(title))
 
@@ -121,15 +139,20 @@ for n in reversed(range(0, x)):
         if DEBUG:
             print(r.content)
         # Save progress before bailing
-        with open(last_file, 'w') as f:
-            f.write(guid)
+        if last_guids:
+            with open(last_file, 'w') as f:
+                f.writelines(last_guids)
         sys.exit(1)
 
-    # Update guid only at this point so that only published entries are saved
-    guid = post['guid']  # e.g. 'file-download-798601'
+    # Update guid at this point so that only published entries are saved
+    last_guids = [
+        "{}\n".format(items[n].get("guid")),
+        "{}\n".format(items[max(n - 1, 0)].get("guid")),
+        "{}\n".format(items[max(n - 2, 0)].get("guid"))
+    ]
 
     sleep(timeout)  # We do not want to be cooled down by Bot API
 
 # Write guid of last posted entry to laststatus.txt
 with open(last_file, 'w') as f:
-    f.write(guid)
+    f.writelines(last_guids)
